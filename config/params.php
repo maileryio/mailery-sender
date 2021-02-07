3d813677120cf95bdeb1ecf135f7610b21a27b8a
Igor Maliy <imaliy.filsh@gmail.com>
2021-02-08 00:12:41 +0200

    Development

--- config/params.php
+++ config/params.php
@@ -1,3 +1,34 @@
 <?php
 
-return [];
+use Yiisoft\Router\UrlGeneratorInterface;
+
+return [
+    'maileryio/mailery-menu-sidebar' => [
+        'items' => [
+            'senders' => [
+                'label' => static function () {
+                    return 'Senders';
+                },
+                'icon' => 'settings',
+                'items' => [
+                    'senders' => [
+                        'label' => static function () {
+                            return 'All Senders';
+                        },
+                        'url' => static function (UrlGeneratorInterface $urlGenerator) {
+                            return $urlGenerator->generate('/sender/sender/index');
+                        },
+                    ],
+                    'domains' => [
+                        'label' => static function () {
+                            return 'Verification Domains';
+                        },
+                        'url' => static function (UrlGeneratorInterface $urlGenerator) {
+                            return $urlGenerator->generate('/sender/domain/index');
+                        },
+                    ],
+                ],
+            ],
+        ],
+    ],
+];
