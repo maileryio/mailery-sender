83fca760fbeba0865da639ec5d9b283362dd4945
Igor Maliy <imaliy.filsh@gmail.com>
2022-04-28 00:20:36 +0300

    Development

--- views/default/index.php
+++ views/default/index.php
@@ -20,34 +20,42 @@ $this->setTitle('All senders');
 
 ?><div class="row">
     <div class="col-12">
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3">
-            <h1 class="h3">All senders</h1>
-            <div class="btn-toolbar float-right">
-                <?= SearchWidget::widget()->form($searchForm); ?>
-                <b-dropdown right size="sm" variant="secondary" class="mb-2">
-                    <template v-slot:button-content>
-                        <?= Icon::widget()->name('settings'); ?>
-                    </template>
-                    <?= ActivityLogLink::widget()
-                        ->tag('b-dropdown-item')
-                        ->label('Activity log')
-                        ->group('sender'); ?>
-                </b-dropdown>
-                <b-dropdown right size="sm" variant="primary" class="mx-sm-1 mb-2">
-                    <template v-slot:button-content>
-                        <?= Icon::widget()->name('plus')->options(['class' => 'mr-1']); ?>
-                        Add new sender
-                    </template>
-                    <?php foreach ($senderTypeList as $senderType) {
-                        echo Html::tag(
-                            'b-dropdown-item',
-                            $senderType->getCreateLabel(),
-                            [
-                                'href' => $url->generate($senderType->getCreateRouteName(), $senderType->getCreateRouteParams()),
-                            ]
-                        );
-                    } ?>
-                </b-dropdown>
+        <div class="card mb-3">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md">
+                        <h4 class="mb-0">All senders</h4>
+                    </div>
+                    <div class="col-auto">
+                        <div class="btn-toolbar float-right">
+                            <?= SearchWidget::widget()->form($searchForm); ?>
+                            <b-dropdown right size="sm" variant="secondary" class="mb-2">
+                                <template v-slot:button-content>
+                                    <?= Icon::widget()->name('settings'); ?>
+                                </template>
+                                <?= ActivityLogLink::widget()
+                                    ->tag('b-dropdown-item')
+                                    ->label('Activity log')
+                                    ->group('sender'); ?>
+                            </b-dropdown>
+                            <b-dropdown right size="sm" variant="primary" class="mx-sm-1 mb-2">
+                                <template v-slot:button-content>
+                                    <?= Icon::widget()->name('plus')->options(['class' => 'mr-1']); ?>
+                                    Add new sender
+                                </template>
+                                <?php foreach ($senderTypeList as $senderType) {
+                                    echo Html::tag(
+                                        'b-dropdown-item',
+                                        $senderType->getCreateLabel(),
+                                        [
+                                            'href' => $url->generate($senderType->getCreateRouteName(), $senderType->getCreateRouteParams()),
+                                        ]
+                                    );
+                                } ?>
+                            </b-dropdown>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -55,67 +63,71 @@ $this->setTitle('All senders');
 <div class="mb-2"></div>
 <div class="row">
     <div class="col-12">
-        <?= GridView::widget()
-            ->layout("{items}\n<div class=\"mb-4\"></div>\n{summary}\n<div class=\"float-right\">{pager}</div>")
-            ->options([
-                'class' => 'table-responsive',
-            ])
-            ->tableOptions([
-                'class' => 'table table-hover',
-            ])
-            ->emptyText('No data')
-            ->emptyTextOptions([
-                'class' => 'text-center text-muted mt-4 mb-4',
-            ])
-            ->paginator($paginator)
-            ->currentPage($paginator->getCurrentPage())
-            ->columns([
-                [
-                    'label()' => ['Name'],
-                    'value()' => [fn (Sender $model) => Html::a($model->getName(), $url->generate($model->getViewRouteName(), $model->getViewRouteParams()))],
-                ],
-                [
-                    'label()' => ['Type'],
-                    'value()' => [static function (Sender $model) use ($senderTypeList) {
-                        $senderType = $senderTypeList->findByEntity($model);
-                        return $senderType ? $senderType->getLabel() : null;
-                    }],
-                ],
-                [
-                    'label()' => ['Status'],
-                    'value()' => [static function (Sender $model) {
-                        return '<span class="ml-2 badge ' . $model->getStatus()->getCssClass() . '">' . $model->getStatus()->getLabel() . '</span>';
-                    }],
-                ],
-                [
-                    'label()' => ['Edit'],
-                    'value()' => [static function (Sender $model) use ($url) {
-                        return Html::a(
-                            Icon::widget()->name('pencil')->render(),
-                            $url->generate($model->getEditRouteName(), $model->getEditRouteParams()),
-                            [
-                                'class' => 'text-decoration-none mr-3',
-                            ]
-                        )
-                        ->encode(false);
-                    }],
-                ],
-                [
-                    'label()' => ['Delete'],
-                    'value()' => [static function (Sender $model) use ($csrf, $url) {
-                        return Link::widget()
-                            ->csrf($csrf)
-                            ->label(Icon::widget()->name('delete')->options(['class' => 'mr-1'])->render())
-                            ->method('delete')
-                            ->href($url->generate($model->getDeleteRouteName(), $model->getDeleteRouteParams()))
-                            ->confirm('Are you sure?')
-                            ->options([
-                                'class' => 'text-decoration-none text-danger',
-                            ])
-                            ->encode(false);
-                    }],
-                ],
-            ]);
-        ?>
+        <div class="card mb-3">
+            <div class="card-body">
+                <?= GridView::widget()
+                    ->layout("{items}\n<div class=\"mb-4\"></div>\n{summary}\n<div class=\"float-right\">{pager}</div>")
+                    ->options([
+                        'class' => 'table-responsive',
+                    ])
+                    ->tableOptions([
+                        'class' => 'table table-hover',
+                    ])
+                    ->emptyText('No data')
+                    ->emptyTextOptions([
+                        'class' => 'text-center text-muted mt-4 mb-4',
+                    ])
+                    ->paginator($paginator)
+                    ->currentPage($paginator->getCurrentPage())
+                    ->columns([
+                        [
+                            'label()' => ['Name'],
+                            'value()' => [fn (Sender $model) => Html::a($model->getName(), $url->generate($model->getViewRouteName(), $model->getViewRouteParams()))],
+                        ],
+                        [
+                            'label()' => ['Type'],
+                            'value()' => [static function (Sender $model) use ($senderTypeList) {
+                                $senderType = $senderTypeList->findByEntity($model);
+                                return $senderType ? $senderType->getLabel() : null;
+                            }],
+                        ],
+                        [
+                            'label()' => ['Status'],
+                            'value()' => [static function (Sender $model) {
+                                return '<span class="ml-2 badge ' . $model->getStatus()->getCssClass() . '">' . $model->getStatus()->getLabel() . '</span>';
+                            }],
+                        ],
+                        [
+                            'label()' => ['Edit'],
+                            'value()' => [static function (Sender $model) use ($url) {
+                                return Html::a(
+                                    Icon::widget()->name('pencil')->render(),
+                                    $url->generate($model->getEditRouteName(), $model->getEditRouteParams()),
+                                    [
+                                        'class' => 'text-decoration-none mr-3',
+                                    ]
+                                )
+                                ->encode(false);
+                            }],
+                        ],
+                        [
+                            'label()' => ['Delete'],
+                            'value()' => [static function (Sender $model) use ($csrf, $url) {
+                                return Link::widget()
+                                    ->csrf($csrf)
+                                    ->label(Icon::widget()->name('delete')->options(['class' => 'mr-1'])->render())
+                                    ->method('delete')
+                                    ->href($url->generate($model->getDeleteRouteName(), $model->getDeleteRouteParams()))
+                                    ->confirm('Are you sure?')
+                                    ->options([
+                                        'class' => 'text-decoration-none text-danger',
+                                    ])
+                                    ->encode(false);
+                            }],
+                        ],
+                    ]);
+                ?>
+            </div>
+        </div>
     </div>
 </div>
